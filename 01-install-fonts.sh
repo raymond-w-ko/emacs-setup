94d01006c06b3bad1bf6ed8ea52a4aa698a182db
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-10-11 13:58:27 -0400

    update to install jetbrains mono

--- 01-install-fonts.sh
+++ 01-install-fonts.sh
@@ -6,4 +6,6 @@ mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 rm -f iosevka-comfy
 ln -s $SCRIPT_DIR/fonts/iosevka-comfy iosevka-comfy
+rm -f jetbrains-mono
+ln -s $SCRIPT_DIR/fonts/jetbrains-mono jetbrains-mono
 fc-cache -fv

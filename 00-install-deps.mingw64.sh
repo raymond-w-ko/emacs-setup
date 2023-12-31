da7db46bd252a58e2d45139d7ccf95de29a53320
Raymond W. Ko <raymond.w.ko@gmail.com>
2023-12-31 03:25:55 -0500

    get emacs to compile in msys2

---
+++ 00-install-deps.mingw64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+pacman -S --needed base-devel \
+mingw-w64-x86_64-toolchain \
+mingw-w64-x86_64-xpm-nox \
+mingw-w64-x86_64-gmp \
+mingw-w64-x86_64-gnutls \
+mingw-w64-x86_64-libtiff \
+mingw-w64-x86_64-giflib \
+mingw-w64-x86_64-libpng \
+mingw-w64-x86_64-libjpeg-turbo \
+mingw-w64-x86_64-librsvg \
+mingw-w64-x86_64-libwebp \
+mingw-w64-x86_64-lcms2 \
+mingw-w64-x86_64-jansson \
+mingw-w64-x86_64-libxml2 \
+mingw-w64-x86_64-zlib \
+mingw-w64-x86_64-harfbuzz \
+mingw-w64-x86_64-libgccjit \
+mingw-w64-x86_64-sqlite3 \
+mingw-w64-x86_64-tree-sitter \
+mingw-w64-x86_64-pcre \
+mingw-w64-x86_64-pcre2
